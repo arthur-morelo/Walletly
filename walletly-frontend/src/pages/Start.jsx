d10f0019bb819cdaf3bbc66e9e4aa9834a0d5a90
arthur-morelo <arthurmorelo@gmail.com>
2026-06-22 16:47:18 -0300

    conserto no extrato

--- walletly-frontend/src/pages/Start.jsx
+++ walletly-frontend/src/pages/Start.jsx
@@ -15,7 +15,7 @@ function Start() {
     const navigate = useNavigate();
 
     return (
-        <div className="flex flex-col min-h-screen bg-white dark:bg-slate-900 transition-colors duration-300">
+        <div className="flex flex-col min-h-screen bg-white transition-colors duration-300">
             {/* Header seguindo a estética do Header.jsx */}
             <Header />
 
